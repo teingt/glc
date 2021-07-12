175a2dbb6521c4d7ec9ceb71693800c47716dcfb
teingt <86691941+teingt@users.noreply.github.com>
2021-07-12 17:53:18 +0300

    Dosyalar

---
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -0,0 +1,45 @@
+package com.hoaxify.ws.user;
+
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.hoaxify.ws.error.NotFoundException;
+
+@Service
+public class UserService {
+	
+	UserRepository userRepository;
+	
+	PasswordEncoder passwordEncoder;
+	
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	public void save(User user) {
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+		userRepository.save(user);
+		
+	}
+
+	public Page<User> getUsers(Pageable page, User user) {
+		if(user != null) {
+			return userRepository.findByUsernameNot(user.getUsername(), page);
+		}
+		return userRepository.findAll(page);
+	}
+
+	public User getByUsername(String username) {
+		User inDB = userRepository.findByUsername(username);
+		if(inDB == null) {
+			throw new NotFoundException();
+		}
+		return inDB;
+	}
+
+}
